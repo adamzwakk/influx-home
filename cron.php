869bbcb9c963363b8be9b77812a389ddeaf522c3
adamzwakk <shamist@gmail.com>
2018-02-10 13:54:55 -0500

    added better cron handling with lib

---
+++ cron.php
@@ -0,0 +1,41 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GO\Scheduler;
+
+define('ROOT_DIR', __DIR__);
+
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+$scheduler = new Scheduler();
+
+$scheduler->call(function () {
+	// Call Nest API Every 5mins
+
+    $n = new Nest();
+	$nestDevices = $n->getDevices();
+	$n->insertHouseTemps();
+	
+})->at('*/5 * * * *');
+
+$scheduler->call(function () {
+	// Call OpenWeather API Every 5mins
+
+    $owm = new OpenWeather();
+$temp = $owm->getOutsideTemp();
+$owm->insertOutsideTemp();
+
+})->at('*/15 * * * *');
+
+
+$scheduler->call(function () {
+	// Call HUE Api every 1min
+
+    $ph = new PhilipsHue();
+	$lightsOnCount = $ph->insertLightsOn();
+
+})->everyMinute();
+
+$scheduler->run();
