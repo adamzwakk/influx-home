c1bc44061e49f1ef1a12da00209a06e9094ffb08
adamzwakk <shamist@gmail.com>
2018-02-09 23:41:22 -0500

    house temps going in influxdb

---
+++ classes/Influx.php
@@ -0,0 +1,27 @@
+<?php
+
+use InfluxDB\Point;
+use InfluxDB\Database;
+
+class Influx{
+
+	protected $client;
+	protected $database;
+
+	public function __construct(){
+		$this->client = new InfluxDB\Client(getenv('INFLUX_IP'), 8086);
+		$this->database = $this->client->selectDB(getenv('INFLUX_DB'));
+	}
+
+	public function insertHouseTemp($ambient,$target){
+		$point = new Point(
+			'house_temp', 
+			$ambient,
+			[],
+			['target_temp' => $target],
+			time()
+		);
+
+		$result = $this->database->writePoints([$point], Database::PRECISION_SECONDS);
+	}
+}
--- start.php
+++ start.php
@@ -7,7 +7,12 @@ define('ROOT_DIR', __DIR__);
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
+$influx = new Influx();
+
 $n = new Nest();
 $nestDevices = $n->getDevices();
 
-echo json_encode($nestDevices);
+foreach($nestDevices->thermostats as $nd){
+	echo "Writing in the current temp ".$nd->ambient_temperature_c."C from ".$nd->name."\n";
+	$influx->insertHouseTemp($nd->ambient_temperature_c, $nd->target_temperature_c);
+}
